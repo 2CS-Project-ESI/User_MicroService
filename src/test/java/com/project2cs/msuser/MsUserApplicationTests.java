4f68a08fbef8fbcd69c584f9fbfe4062f168e3c3
Assil KAHLERRAS <a.kahlerras@esi-sba.dz>
2025-03-09 09:23:02 +0100

    first

---
+++ src/test/java/com/project2cs/msuser/MsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project2cs.msuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
